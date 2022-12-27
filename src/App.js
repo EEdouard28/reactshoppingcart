debca3a31be18fdd332a6a4212d391e548434cd1
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-26 21:04:56 -0500

    rows and cols added to store

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import NavbarComponent from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cancel from './pages/Cancel';
+import Success from './pages/Success';
+import Store from './pages/Store';
 
 function App() {
   return (
@@ -11,7 +14,9 @@ function App() {
       <NavbarComponent></NavbarComponent>
       <BrowserRouter>
         <Routes>
+          {/* base root below */}
           <Route index element={<Store />} />
+          {/* success and cancel components */}
           <Route path="success" element={<Success />} />
           <Route path="cancel" element={<Cancel />} />
         </Routes>

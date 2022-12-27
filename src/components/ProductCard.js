f5c9dd688f0dcb788e61f88b0b96175065062adc
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-26 21:18:02 -0500

    productcard component created

---
+++ src/components/ProductCard.js
@@ -0,0 +1,2 @@
+function ProductCard() {}
+export default ProductCard;
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -4,7 +4,9 @@ import { productsArray } from '../productsStore';
 function Store() {
   return (
     <>
-      <h1>Welcome to the store!</h1>
+      <h1 align="center" className="p-3">
+        Welcome to the store!
+      </h1>
       <Row xs={1} md={3} className="g-4">
         {productsArray.map((product, idx) => (
           <Col align="center" key={idx}>

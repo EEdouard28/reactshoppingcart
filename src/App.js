e20cb2bf17498c91a168c60782dfa50db0c69f10
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-26 20:57:43 -0500

    cancel page added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import NavbarComponent from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Container>
       <NavbarComponent></NavbarComponent>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }
